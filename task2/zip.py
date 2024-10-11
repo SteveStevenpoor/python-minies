6e3c08dfcdbba74240095ac258cfa18bc244c337
SteveStevenpoor <eeeevaeee@gmail.com>
2024-10-11 20:09:37 +0700

    fixed boilerplate code

--- task2/zip.py
+++ task2/zip.py
@@ -1,10 +1,4 @@
 # This function creates mapped list of 2 lists
 
 def my_zip(l1: list, l2: list) -> list:
-    result = []
-    n = len(l1) if len(l1) < len(l2) else len(l2)
-
-    for i in range(n):
-        result.append((l1[i], l2[i]))
-
-    return result
+    return [(l1[i], l2[i]) for i in range(min(len(l1), len(l2)))]
