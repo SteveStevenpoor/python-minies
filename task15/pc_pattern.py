8bcae60a3111f1dad412bafb5f77b953d6ef8df3
SteveStevenpoor <eeeevaeee@gmail.com>
2024-12-28 17:17:36 +0700

    change queue structure

--- task15/pc_pattern.py
+++ task15/pc_pattern.py
@@ -1,20 +1,27 @@
-import queue
-from random import randint
-from threading import Thread
+from collections import deque
+from threading import Thread, Lock
+from time import sleep, time
 
 import numpy as np
-from scipy.sparse.linalg import matrix_power
+from numpy.linalg import matrix_power
 
-task_queue = queue.Queue()
+task_queue = deque()
+queue_lock = Lock()
 THREAD_COUNT = 5
 
 
 def producer():
     size = 1
-    while True:
-        task = (size, randint(1, 3), randint(1, 5))
-        task_queue.put(task)
+    for _ in range(0, 100, 1):
+        task = (size, 2, 2)
+        with queue_lock:
+            task_queue.append(task)
         size += 1
+        sleep(0.1)
+
+    for _ in range(THREAD_COUNT):
+        with queue_lock:
+            task_queue.append(None)
 
 
 def process(size: int, value: int, times: int) -> int:
@@ -26,19 +33,24 @@ def process(size: int, value: int, times: int) -> int:
 
 def consumer():
     while True:
-        task = task_queue.get()
-        print(f'Task: {task}, result: {process(*task)}')
+        task = None
+        with queue_lock:
+            if task_queue:
+                task = task_queue.popleft()
+                if task is None:
+                    break
+        if task: print(f'Task: {task}, result: {process(*task)}')
 
-
+begin = time()
 producer = Thread(target=producer, args=[])
 producer.start()
 
 thread_pool = list()
 for i in range(THREAD_COUNT):
-    consumer = Thread(target=consumer, args=[])
-    thread_pool.append(consumer)
-    consumer.start()
+    thread_pool.append(Thread(target=consumer, args=[]))
+    thread_pool[-1].start()
 
 producer.join()
 for thr in thread_pool:
     thr.join()
+print(time() - begin)
